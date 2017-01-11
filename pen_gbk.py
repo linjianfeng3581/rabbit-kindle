520baf9035ffa09ff95ccf2b6711b7388e4650ff
Hanke <820525831@qq.com>
2017-01-11 20:32:14 +0800

    删除解压代码

--- pen_gbk.py
+++ pen_gbk.py
@@ -25,57 +25,8 @@ today = datetime.date.today().isoformat()
 logFile = open(logDir + "log-" + today + ".txt", "a+")
 
 
-def del_file(delete_file):
-    if os.path.exists(delete_file):
-        os.remove(delete_file)
-
-
-def un_rar(ful_file_name):
-    r_file = rarfile.RarFile(ful_file_name)
-    dir_name = os.path.splitext(ful_file_name)[0]
-    if os.path.isdir(dir_name):
-        pass
-    else:
-        os.mkdir(dir_name)
-    os.chdir(dir_name)
-    try:
-        r_file.extractall()
-    except Exception, e:
-        print>> logFile, ful_file_name, e
-        pass
-    r_file.close()
-    del_file(ful_file_name)
-
-
-def un_zip(ful_file_name):
-    z_file = zipfile.ZipFile(ful_file_name, 'r')
-    dir_name = os.path.splitext(ful_file_name)[0]
-    if os.path.isdir(dir_name):
-        pass
-    else:
-        os.mkdir(dir_name)
-    for files in z_file.namelist():
-        try:
-            z_file.extract(files, dir_name)
-        except Exception, e:
-            print>> logFile, ful_file_name, e
-            pass
-    z_file.close()
-    del_file(ful_file_name)
-
-
-# 第一次遍历，就解压缩文件，然后将压缩文件删除
-# for parent, _, filenames in os.walk(rotDir + srcDir):
-#     for filename in filenames:
-#         print>> logFile, filename
-#         ext = os.path.splitext(filename)[1].lower()
-#         if ext == ".rar":
-#             un_rar(os.path.join(parent, filename))
-#         if ext == ".zip":
-#             un_zip(os.path.join(parent, filename))
-
 ignoreExtends = ['.rar', '.zip', '.7z', '.tar', '.iso']
-deleteExtends = ['', '.db', '.ebk3', '.003zip', '.flv', '.downloading', '.014', '.chm', '.jpg', '.006', '.torrent',
+deleteExtends = [ '.db', '.ebk3', '.003zip', '.flv', '.downloading', '.014', '.chm', '.jpg', '.006', '.torrent',
                  '.ps', '.htm', '.019', '.011', '.010', '.xlsx', '.012', '.015', '.017', '.016', '.prc', '.log', '.url',
                  '.018', '.cue', '.se!', '.html', '.dkp', '.uvz', '.020', '.umd', '.008', '.009', '.007',
                  '.004', '.002', '.003', '.mbp', '.ape', '.cfg', '.pdb', '.013', '.exe', '.opf']
