99213d99b9497293b85dd0d8617ccf127b843d1b
Hanke <820525831@qq.com>
2017-01-14 11:37:05 +0800

    删除空文件夹

---
+++ remove_empty.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# coding:utf8
+
+import os.path
+import datetime
+
+import sys
+
+reload(sys)
+sys.setdefaultencoding('gbk')
+
+rotDir = "D:/"
+
+srcDir = "kindle/2017-01-04"
+disDir = "org-kindle/"
+logDir = "log/"
+extends = []
+
+today = datetime.date.today().isoformat()
+logFile = open(rotDir + logDir + "log-" + today + ".txt", "a+")
+
+# 删除空文件夹
+for parent, _, filenames in os.walk(rotDir + srcDir):
+    if not os.listdir(parent):
+        os.rmdir(parent)
